998bcd553e9ab8342755a988f6b6cee3c140d699
mohit <mohitkumawat221004@gmail.com>
2025-07-18 20:40:11 +0530

    server set

--- urlshortner/app.js
+++ urlshortner/app.js
@@ -1,22 +1,26 @@
 const express= require('express');
-const path = ("path");
+const path = require("path");
+const PORT = process.env.PORT || 5000;
 const app = express();
 // import path from "path";
 
-// // static use
-app.use(express.static("public"));
+// // // static use
+// app.use(express.static("public"));
 
 // // // router use
 // app.use(rout);
+app.use(express.json());
 
+const staticPath = path.join(__dirname, 'public');
+app.use(express.static(staticPath));
 // // get method
 app.get("/", (req, res) => {
-    res.send("HELLO");
+    res.sendFile (path.resolve(__dirname,"public","URL.html"));
 })
 
 // const staticPath = path.join(import.meta.dirname, "new");
 // app.use(express.static(staticPath));
 
-app.listen(process.env.PORT, () => {
-    console.log(`server is running at port: ${process.env.PORT}`);
+app.listen(PORT, () => {
+    console.log(`server is running at port: http://localhost:${PORT}`);
 });
