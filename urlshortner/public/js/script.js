d08b930e0ac79281980f5f1e8e65433793a3d10c
mohit <mohitkumawat221004@gmail.com>
2025-07-19 22:50:27 +0530

    none

--- urlshortner/public/js/script.js
+++ urlshortner/public/js/script.js
@@ -3,7 +3,14 @@ const shorten = document.getElementById("shorten-form");
 
 const first = document.querySelectorAll(".first");
 first.addEventListener("onclick", (event) => {
-    
+    event.preventDefault();
+});
+
+const submitBtn = document.querySelector('.submitBtn');
+
+submitBtn.addEventListener("onclick", (event) => {
+    event.preventDefault();
+
 })
 shorten.addEventListener("submit", (event) => {
     event.preventDefault();
