ade881876d9b7facbc7f6558f674d27713e206bb
mohit <mohitkumawat221004@gmail.com>
2025-07-19 22:44:03 +0530

    one

--- urlshortner/public/js/script.js
+++ urlshortner/public/js/script.js
@@ -1,4 +1,10 @@
 const shorten = document.getElementById("shorten-form");
+
+
+const first = document.querySelectorAll(".first");
+first.addEventListener("onclick", (event) => {
+    
+})
 shorten.addEventListener("submit", (event) => {
     event.preventDefault();
     const formData = new FormData(event.target);
