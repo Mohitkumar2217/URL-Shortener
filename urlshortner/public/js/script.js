436fe2d2012763148420f65e9223446b5e78e95a
mohit <mohitkumawat221004@gmail.com>
2025-07-21 17:44:14 +0530

    changes

--- urlshortner/public/js/script.js
+++ urlshortner/public/js/script.js
@@ -1,21 +1,23 @@
 const shorten = document.getElementById("shorten-form");
 
-
-const first = document.querySelectorAll(".first");
-first.addEventListener("onclick", (event) => {
-    event.preventDefault();
-});
-
-const submitBtn = document.querySelector('.submitBtn');
-
-submitBtn.addEventListener("onclick", (event) => {
-    event.preventDefault();
-
-})
 shorten.addEventListener("submit", (event) => {
+
     event.preventDefault();
     const formData = new FormData(event.target);
+    
     const url = formData.get("url");
     const shortcode = formData.get("shortCode");
     console.log(url,shortcode);
 });
+
+// const first = document.querySelectorAll(".first");
+// first.addEventListener("onclick", (event) => {
+//     event.preventDefault();
+// });
+
+// const submitBtn = document.querySelector('.submitBtn');
+
+// submitBtn.addEventListener("onclick", (event) => {
+//     event.preventDefault();
+
+// })
