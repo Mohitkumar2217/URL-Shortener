3411cdef9d839c211c3e8b9b1ad7984e30085a2c
Mohit kumawat <mohitkumawat221004@gmail.com>
2025-08-08 19:42:00 +0530

    update

---
+++ controllers/analytic.js
@@ -0,0 +1,15 @@
+const URL = require("../models/url");
+
+async function handleAnalyticLink(req, res) {
+    const shortId = req.params.shortId;
+    const result = await URL.findOne({ shortId });
+
+    return res.json({
+        totalClicks: result.visitHistory.length,
+        analytics: result.visitHistory,
+    });
+}
+
+module.exports = {
+    handleAnalyticLink,
+}
--- routes/geturl.js
+++ routes/geturl.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const { handleShortLink } = require('../controllers/redirect');
+const { handleAnalyticLink } = require('../controllers/analytic');
 
 const router = express.Router();
 
 router.get("/:shortId", handleShortLink) ;
+router.get("/analytic/:shortId", handleAnalyticLink);
 
 module.exports = router;
