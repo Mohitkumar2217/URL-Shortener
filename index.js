f5718df3bbb3d06d8374a1cf4bb6aa710c244923
Mohit kumawat <mohitkumawat221004@gmail.com>
2025-08-08 18:43:21 +0530

    update

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 const express = require("express");
 const urlRoute = require("./routes/posturl");
+const { connectToMongoDB } = require("./connect");
 const app = express();
 const PORT = process.env.PORT || 8001;
+
+app.use("/url", urlRoute);
+
+connectToMongoDB("mongodb://127.0.0.1:27017/urlshortener")
+.then(() => {
+    console.log("MongoDB is connected");
+})
+.catch((err) => {
+    console.log(err);
+});
 
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
