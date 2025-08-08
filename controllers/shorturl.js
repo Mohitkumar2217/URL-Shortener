d9457f7c5b115d055385f4c70f80afce326708f4
Mohit kumawat <mohitkumawat221004@gmail.com>
2025-08-08 18:26:08 +0530

    new update

---
+++ controllers/shorturl.js
@@ -0,0 +1,23 @@
+const {nanoid} = require("nanoid");
+const URL = require("../models/url");
+
+async function handleLoadLink(req, res) {
+    const body = req.body;
+    if(!body.url) return res.status(400).json({ error: "url is required"});
+    const shortID = nanoid(8);
+    try {
+        await URL.create({
+            shortId: shortId,
+            redirectId: body.url,
+            visitHistory: [],
+        })
+        return res.json({ id: shortID });
+    }
+    catch(error) {
+        return res.status(500).json({ msg: `error: ${error}` })
+    }
+}
+
+module.exports = {
+    handleLoadLink,
+}
