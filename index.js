c0d258c5f3b192fdda51e3c586bd00ec22e70286
Mohit kumawat <mohitkumawat221004@gmail.com>
2025-08-09 17:25:24 +0530

    ejs

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const path = require("path");
+const URL = require('./models/url');
 const urlPostRoute = require("./routes/posturl");
 const urlGetRoute = require("./routes/geturl");
 const { connectToMongoDB } = require("./connect");
@@ -8,13 +10,25 @@ const PORT = process.env.PORT || 8001;
 
 dotenv.config();
 
+connectToMongoDB(process.env.MONGO_URI).then(() => {
+    console.log("MongoDB is connected");
+});
+
+app.set("view engine", "ejs");
+app.set("views", path.resolve("./views"));
+
+app.get("/test", async (req, res) => {
+    const allUrls = await URL.find({});
+    res.render("home" , {
+        urls: allUrls,
+    });
+})
+
 app.use(express.json());
 app.use("/url", urlPostRoute);
 app.use("/url", urlGetRoute);
 
-connectToMongoDB(process.env.MONGO_URI).then(() => {
-    console.log("MongoDB is connected");
-});
+
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
 })
