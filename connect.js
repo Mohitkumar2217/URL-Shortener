b1527d27190d03a54690e07b7aaca785fa8a0c69
Mohit kumawat <mohitkumawat221004@gmail.com>
2025-08-09 16:44:36 +0530

    serevr side rendering

--- connect.js
+++ connect.js
@@ -1,8 +1,14 @@
 const mongoose = require("mongoose");
 mongoose.set("strictQuery", true);
-async function connectToMongoDB(url) {
-    return mongoose.connect(url);
-}
+const connectToMongoDB = async (url) => {
+  try {
+    await mongoose.connect(url);
+    console.log('Connected to MongoDB');
+  } catch (err) {
+    console.error('MongoDB connection error:', err);
+  }
+};
+
 
 module.exports = {
     connectToMongoDB,
--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 const express = require("express");
+const dotenv = require("dotenv");
 const urlPostRoute = require("./routes/posturl");
 const urlGetRoute = require("./routes/geturl");
 const { connectToMongoDB } = require("./connect");
 const app = express();
 const PORT = process.env.PORT || 8001;
 
+dotenv.config();
+
 app.use(express.json());
 app.use("/url", urlPostRoute);
 app.use("/url", urlGetRoute);
 
-connectToMongoDB("mongodb://127.0.0.1:27017/urlshortener").then(() => {
+connectToMongoDB(process.env.MONGO_URI).then(() => {
     console.log("MongoDB is connected");
 });
 app.listen(PORT, () => {
