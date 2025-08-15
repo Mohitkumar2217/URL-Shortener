2b75cb23115d567f8b8b2b578c4af462e73180c3
Mohit kumawat <mohitkumawat221004@gmail.com>
2025-08-15 16:52:34 +0530

    update

--- services/auth.js
+++ services/auth.js
@@ -1,11 +1,25 @@
-const sessionIdToUserMap = new Map();
+const jwt = require("jsonwebtoken");
+const secret = "Mohitkava$32";
+// For statefull auth
+// const sessionIdToUserMap = new Map();
 
-function setUser(id, user) {
-    sessionIdToUserMap.set(id, user);
+
+function setUser(user) {
+    const payload = {
+        _id: user._id,
+        email: user.email,
+    }
+    return jwt.sign(payload, secret);
+    // sessionIdToUserMap.set(id, user);
 }
 
-function getUser(id) {
-    return sessionIdToUserMap.get(id);
+function getUser(token) {
+    if(!token) return null;
+    try {
+        return jwt.verify(token, secret);
+    } catch (error) {
+        return null;
+    }
 }
 
 
