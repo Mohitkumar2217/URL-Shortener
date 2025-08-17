784b9f2e1944128b9a9cc8f68c31c8e60db3b451
Mohit kumawat <mohitkumawat221004@gmail.com>
2025-08-17 19:09:37 +0530

    update

--- index.js
+++ index.js
@@ -21,13 +21,14 @@ connectToMongoDB(process.env.MONGO_URI || "mongodb://127.0.0.1:27017/urlshortene
 });
 
 app.set("view engine", "ejs");
-app.set("views", path.resolve("./views"));
+app.set('views', path.resolve('./public/views'));
 
 // middlewares
 app.use(express.json());
 app.use(express.urlencoded({ extended: false}));
 app.use(cookieParser());
 app.use(checkforAuthentication);
+app.use(express.static('public'));
 
 // routes middlewares
 app.use("/url",restrictTo(['NORMAL', "ADMIN"]), urlPostRoute);
