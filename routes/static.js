0d804ada440393c3e92863e36d1aaffe9bdaf719
Mohit kumawat <mohitkumawat221004@gmail.com>
2025-09-06 09:27:52 +0530

    update

--- routes/static.js
+++ routes/static.js
@@ -29,7 +29,7 @@ router.get("/login", async (req, res) => {
 
 // Server-side route (optional, for token blacklist)
 router.get('/logout', async (req, res) => {
-  res.clearCookie('token'); // or whatever your cookie name is
+  res.clearCookie('token'); // token delete to logout
   return res.redirect('/login');
 })
 
