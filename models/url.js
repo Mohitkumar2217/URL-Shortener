fe15cdc369bc6ab874246956e92d0a683bf86bb2
Mohit kumawat <mohitkumawat221004@gmail.com>
2025-11-15 14:55:39 +0530

    update dependecies

--- models/url.js
+++ models/url.js
@@ -1,27 +1,39 @@
+// models/url.js
 const mongoose = require("mongoose");
 
-const urlSchema = new mongoose.Schema({
-    shortId: {
-        type: String,
-        required: true,
-        unique: true
-    },
+// Basic URL validator
+function isValidUrl(val) {
+  try {
+    new URL(val);
+    return true;
+  } catch {
+    return false;
+  }
+}
+
+const urlSchema = new mongoose.Schema(
+  {
+    shortId: { type: String, required: true, unique: true, trim: true },
     redirectId: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
+      validate: {
+        validator: isValidUrl,
+        message: (props) => `${props.value} is not a valid URL!`,
+      },
     },
-    visitHistory: [{
-        timestamp: {
-            type: Number
-        }
-    }],
-    createdBy: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "users",
-    }
-}, 
-{timestamps: true});
+    visitHistory: [
+      {
+        timestamp: { type: Number, default: () => Date.now() },
+        ip: { type: String },
+        userAgent: { type: String },
+        referrer: { type: String },
+      },
+    ],
+    createdBy: { type: mongoose.Schema.Types.ObjectId, ref: "user", required: false },
+  },
+  { timestamps: true }
+);
 
 const URL = mongoose.model("url", urlSchema);
-
 module.exports = URL;
--- models/user.js
+++ models/user.js
@@ -1,34 +1,41 @@
-const mongoose= require('mongoose');
+// models/user.js
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+
+const SALT_ROUNDS = 10;
+
 const userSchema = new mongoose.Schema(
-    {
-        name: {
-            type: String,
-            required: true,
-        },
-        email: {
-            type: String,
-            required: true,
-            unique: true
-        },
-        role: {
-            type: String,
-            required: true,
-            default: "NORMAL",
-        },
-        password: {
-            type: String,
-            required: true,
-        },
-        confirmpass: {
-            type: String,
-            required: true,
-        }
+  {
+    name: { type: String, required: [true, "Name is required"], trim: true },
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+      unique: true,
+      trim: true,
+      lowercase: true,
+      match: [/\S+@\S+\.\S+/, "Invalid email address"],
     },
-    {
-        timestamps: true
-    }
+    role: { type: String, enum: ["NORMAL", "ADMIN"], default: "NORMAL" },
+    password: { type: String, required: [true, "Password is required"] },
+    confirmpass: { type: String, required: [true, "Password is required"] },
+  },
+  { timestamps: true }
 );
 
-const User = mongoose.model("user", userSchema);
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  try {
+    const hashed = await bcrypt.hash(this.password, SALT_ROUNDS);
+    this.password = hashed;
+    next();
+  } catch (err) {
+    next(err);
+  }
+});
 
-module.exports = User;    
+userSchema.methods.comparePassword = function (candidatePassword) {
+  return bcrypt.compare(candidatePassword, this.password);
+};
+
+const User = mongoose.model("user", userSchema);
+module.exports = User;
