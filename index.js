fdfd54c0585acdfb8215836b27d895b46839af44
Mohit kumawat <mohitkumawat221004@gmail.com>
2025-11-21 11:40:24 +0530

    updates of url shortner

--- index.js
+++ index.js
@@ -9,28 +9,30 @@ dotenv.config();
 
 const PORT = process.env.PORT;
 // middle ware
-const {checkforAuthentication, restrictTo} = require("./middlewares/auth");
+const { checkforAuthentication, restrictTo } = require("./middlewares/auth");
 // routes
 const staticRouter = require("./routes/static");
 const urlPostRoute = require("./routes/posturl");
 const urlGetRoute = require("./routes/geturl");
 const userRoute = require("./routes/user");
- 
-connectToMongoDB(process.env.MONGO_URI);
+
+connectToMongoDB("mongodb://127.0.0.1:27017/urlshortener").then(() => {
+    console.log("MongoDB is connected");
+});
 
 app.set("view engine", "ejs");
 app.set('views', path.resolve('./public/views'));
 
 // middlewares
 app.use(express.json());
-app.use(express.urlencoded({ extended: false}));
+app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(checkforAuthentication);
 app.use(express.static(path.join(__dirname, "public")));
 // routes middlewares
-app.use("/url",restrictTo(['NORMAL', "ADMIN"]), urlPostRoute);
-app.use("/url",restrictTo(['NORMAL', "ADMIN"]), urlGetRoute);
-app.use("/user", userRoute); 
+app.use("/url", restrictTo(['NORMAL', "ADMIN"]), urlPostRoute);
+app.use("/url", restrictTo(['NORMAL', "ADMIN"]), urlGetRoute);
+app.use("/user", userRoute);
 app.use("/", staticRouter);
 
 app.listen(PORT, () => {
